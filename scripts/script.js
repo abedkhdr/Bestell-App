98642de0a17d8bd190a66f454d17bb8e6faaa1d5
abedkhdr <abedkhodor@outlook.de>
2026-09-14 21:18:49 +0200

    fixed index, script and style

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,11 @@ function renderCategories() {
   for (let i = 0; i < categories.length; i++) {
     menu.innerHTML += `  
 <div class="category-section">
+<div class="category-content">
+
 <img src="${categories[i].image}" alt="${categories[i].name} icon">
 <h2>${categories[i].name}</h2>
+</div>
 </div>`;
 
     for (let j = 0; j < categories[i].products.length; j++) {
@@ -19,7 +22,7 @@ function renderCategories() {
 <p>${categories[i].products[j].description}</p>
 </div>
 <div class="product-right" >
-<p>${categories[i].products[j].price} €</p>
+<p>${categories[i].products[j].price.toFixed(2)} €</p>
 <button type="button">Add to basket </button>
 </div>
 </div>
